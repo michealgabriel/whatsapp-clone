46396fdfbdcc58b6b82fd7770304bb5a0241d90e
michealgabriel <michealgabriel265@gmail.com>
2022-12-07 21:39:57 +0100

    Update endpoint value

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,7 +2,7 @@
  import axios from 'axios';
 
  const instance = axios.create({
-    baseURL: "https://whatsapp-clone-backendserver.herokuapp.com",
+    baseURL: "https://whatsapp-clone-backend.onrender.com",
     headers: {
        'Access-Control-Allow-Origin': '*',
     }
